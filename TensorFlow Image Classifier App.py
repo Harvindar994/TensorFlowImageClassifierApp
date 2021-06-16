c8f852c616cc9f8ccb1750b1fc02f8a557a48ded
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2021-06-16 18:36:54 +0530

    Creating our own neural net model

--- TensorFlow Image Classifier App.py
+++ TensorFlow Image Classifier App.py
@@ -15,10 +15,19 @@ fashion_mnist = keras.datasets.fashion_mnist
 # here pulling the data from dataset.
 (train_image, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-# show data
-print(train_labels[0])
+# # show data
+# for index in range(0, len(train_image)):
+#     print(train_labels[index])
+#     plt.imshow(train_image[index], cmap='gray', vmin=0, vmax=255)
+#     plt.show()
 
-for img, label in train_image, train_labels:
-    print(label)
-    plt.imshow(img, cmap='gray', vmin=0, vmax=255)
-    plt.show()
+
+# define out neural net structure.
+model = keras.Sequentail([
+
+    # input is 28x28 image ("Flatten" flattens the 28x28 into a single input 28*28  = 784 input layer
+    keras.layers.Flatten(input_shape=(28, 28)),
+
+    
+
+])
